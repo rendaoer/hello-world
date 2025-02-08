00cae2444b363d5656e8159f075f8c23dc597485
RenDaoEr <rendaoer@outlook.com>
2025-02-08 11:15:51 +0800

    aaa

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
-/// A "Hello, world!" program.
-///
-/// This is the best implementation of this program to ever exist.
+/// aaa
 
-/// The main function runs when our program starts
 fn main() {
     print("Hello, world!");
 }
 
-// a function that prints a message
 fn print(m: &str) {
     println!("{m}");
 }
